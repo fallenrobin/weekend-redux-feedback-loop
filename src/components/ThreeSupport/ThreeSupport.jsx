46a922a5925091607f3cb5edd249bc3228ee1615
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-03 00:39:09 -0500

    change slider to icons in ThreeSupport

--- src/components/ThreeSupport/ThreeSupport.jsx
+++ src/components/ThreeSupport/ThreeSupport.jsx
@@ -3,21 +3,55 @@ import { useSelector, useDispatch } from 'react-redux';
 import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom';
 
 import Typography from '@mui/material/Typography';
-import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
-import Slider from '@material-ui/core/Slider';
+// import Slider from '@material-ui/core/Slider';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
 
-// const useStyles = makeStyles((theme: Theme) =>
-//     createStyles({
-//         root: {
-//             '& > *': {
-//                 margin: theme.spacing(1),
-//             },
-//         },
-//     }),
-// );
+const StyledRating = withStyles({
+  iconFilled: {
+      color: '#ff6d75',
+  },
+  iconHover: {
+      color: '#ff3d47',
+  },
+})(Rating);
+
+const customIcons = {
+  1: {
+      icon: <VolunteerActivismOutlinedIcon />,
+      label: '1',
+  },
+  2: {
+      icon: <VolunteerActivismOutlinedIcon />,
+      label: '2',
+  },
+  3: {
+      icon: <VolunteerActivismOutlinedIcon />,
+      label: '3',
+  },
+  4: {
+      icon: <VolunteerActivismOutlinedIcon />,
+      label: '4',
+  },
+  5: {
+      icon: <VolunteerActivismOutlinedIcon />,
+      label: '5',
+  },
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
 
 const newLocal = 200;
@@ -68,7 +102,7 @@ function ThreeSupport() {
   const handleSubmit = (event) => {
     console.log('clicked into handleSubmit!', newSupport);
     if (newSupport === 0) {
-      alert('Please use the slider to select a number to continue!')
+      alert('Please enter your level of supportnacity to continue!')
     } else {
       dispatch({
         type: 'ADD_SUPPORT',
@@ -88,7 +122,7 @@ function ThreeSupport() {
 
       <div >
 
-        <Typography id="discrete-slider-custom" gutterBottom>
+        {/* <Typography id="discrete-slider-custom" gutterBottom>
           Pick 1 through 5
         </Typography>
         <Box sx={{ width: 300 }}>
@@ -101,6 +135,16 @@ function ThreeSupport() {
             marks
             min={1}
             max={5}
+            onChange={(event, newValue) => setNewSupport(newValue)}
+          />
+        </Box> */}
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <Rating
+            name="customized-icons"
+            defaultValue={0}
+            value={newSupport}
+            getLabelText={(value) => customIcons[value].label}
+            IconContainerComponent={IconContainer}
             onChange={(event, newValue) => setNewSupport(newValue)}
           />
         </Box>
